43cf8d0cd58e6c54751d83d71ac29c13b3f93b0e
madhurjyaborkotoky <madhurjyaborkotoky@gmail.com>
2026-08-13 15:15:11 +0530

    created the folder images and added a new image.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print('hello world')
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/home/user/dl_madhurjya/deeplearning_campusx/datasets/placement.csv')
+
+print(df.shape)
+df.head()
+
+plt.figure(figsize=(10,6))
+sns.scatterplot(x='cgpa',y='resume_score',data=df, hue='placed')
+plt.savefig("images/my_first_plot.png", dpi=300, bbox_inches="tight")
