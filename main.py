a10a97295dc1f9bf7d77efa50940b3f7b500d09b
madhurjyaborkotoky <madhurjyaborkotoky@gmail.com>
2026-08-13 15:28:36 +0530

    edited the main.py file.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 print('hello world')
 
-
+#importing libraries
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+#reading the dataset
 df = pd.read_csv('/home/user/dl_madhurjya/deeplearning_campusx/datasets/placement.csv')
 
+#getting the shape and brief idea of the dataset
 print(df.shape)
-df.head()
+print(df.head())
 
+#plotting the scatter plot of cgpa vs resume score
 plt.figure(figsize=(10,6))
 sns.scatterplot(x='cgpa',y='resume_score',data=df, hue='placed')
-plt.savefig("images/my_first_plot.png", dpi=300, bbox_inches="tight")
+plt.savefig("images/dataset_scatter_plot.png", dpi=300, bbox_inches="tight")
+
