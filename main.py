8e3a29d0d91dd59ce645270949b3961cd204aad2
madhurjyaborkotoky <madhurjyaborkotoky@gmail.com>
2026-08-13 15:53:50 +0530

    edited the main.py file.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 print('hello world')
 
 #importing libraries
+from pprint import pp
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.linear_model import Perceptron
+from mlxtend.plotting import plot_decision_regions
+
 
 #reading the dataset
 df = pd.read_csv('/home/user/dl_madhurjya/deeplearning_campusx/datasets/placement.csv')
@@ -16,5 +21,19 @@ print(df.head())
 #plotting the scatter plot of cgpa vs resume score
 plt.figure(figsize=(10,6))
 sns.scatterplot(x='cgpa',y='resume_score',data=df, hue='placed')
-plt.savefig("images/dataset_scatter_plot.png", dpi=300, bbox_inches="tight")
+plt.savefig("images/dataset_scatter_plot.png", dpi=600, bbox_inches="tight")
 
+#splitting the dataset into features and target variable
+x = df.iloc[:,0:2]
+y = df.iloc[:,-1]
+
+#creating the perceptron model and fitting it to the dataset
+p = Perceptron()
+p.fit(x, y)
+
+#calculating the weight and bias of the model
+print(p.coef_)
+print(p.intercept_)
+
+plot_decision_regions(x.values, y.values, clf=p)
+plt.savefig("images/decision_regions.png", dpi=300, bbox_inches="tight")
